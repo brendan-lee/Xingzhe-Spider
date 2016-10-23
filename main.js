d013cdd5393029d4098cac228460bad01ea2afe9
LZM <lizheminglzm@gmail.com>
2016-10-23 17:01:27 +0800

    日期范围-前端

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+/**
+ * 提交数据
+ */
 function submit() {
 	//$('body').html('<p>正在爬取GPX数据，过程视数据量可能持续数秒至数分钟，请不要关闭页面。</p>');
 	//$('title').html('正在爬取GPX数据……');
@@ -5,12 +8,15 @@ function submit() {
 	$.post('validate.php', {
 		sessionid: $('#sessionid').val()
 	}, function(data) {
-		setTimeout(function() {
-			$('body').append(data);
-		}, 500);
+		$('#wrapper').append(data);
 	})
 }
 
+/**
+ * 显示信息窗口
+ * @param {Object} title 标题
+ * @param {Object} cont 内容
+ */
 function msg(title, cont) {
 	var msg = $('#pop_msg');
 	var bg = $('#pop_back');
@@ -26,6 +32,10 @@ function msg(title, cont) {
 
 }
 
+/**
+ * 关闭信息窗口
+ * @param {Object} obj 调用该方法的object
+ */
 function closeMsg(obj) {
 	if(event.target != obj) return;
 
@@ -37,3 +47,45 @@ function closeMsg(obj) {
 		bg.css('visibility', 'hidden');
 	})
 }
+
+/**
+ * 动态生成年份/月份
+ */
+(function() {
+	var fromY = $('#from_year'),
+		toY = $('#to_year');
+	var fromM = $('#from_month'),
+		toM = $('#to_month');
+
+	var date = new Date();
+	var curY = date.getFullYear();
+	var curM = date.getMonth() + 1;
+
+	// 写入年份
+	for(var i = 2012; i <= curY; i++) {
+		fromY.append('<option>' + i + '</option>');
+		toY.append('<option>' + i + '</option>');
+		month(fromY, fromM);
+		month(toY, toM);
+	}
+
+	// 年份改变时动态生成月份
+	fromY.on('change', function() {
+		month(fromY, fromM);
+	});
+	toY.on('change', function() {
+		month(toY, toM);
+	});
+
+	/**
+	 * 根据年份生成月份
+	 */
+	function month(y, m) {
+		m.html('');
+		var selY = y.find('option:selected').val();
+		var mLim = selY == curY ? curM : 12;
+		for(var i = 1; i <= mLim; i++) {
+			m.append('<option>' + i + '</option>');
+		}
+	}
+}())
--- validate.php
+++ validate.php
@@ -11,19 +11,19 @@ if ($_POST ['sessionid'] == NULL)
 	exit ( '<script>msg("错误", "sessionid不能为空，请正确填写后执行。")</script>' );
 $sessionId = $_POST ['sessionid'];
 
-
 // sessionid有误
 if (getGPX(1) == '登录以后才能导出')
 	exit('<script>msg("错误", "sessionid不正确，无法登录到行者。")</script>');
 
+echo '<script>$("#wrapper").html("");</script>';
+echo '正在爬取GPX数据，过程视数据量可能耗时数秒至数分钟，请不要关闭页面。';
+
+
+
 define('VALIDATED', 1);
-
-
-
-
 // $jsonString = file_get_contents ( 'http://www.imxingzhe.com/api/v3/user_month_info?user_id=137311&year=2016&month=3' );
 $jsonString = file_get_contents ( 'example.json' );
-$allTracksArr = json_decode ( $jsonString, true ) ['data'] ['wo_info'];
+$allTracksArr = json_decode ( $jsonString, true )['data']['wo_info'];
 
 // 所有要抓取的轨迹
 $trackId = array (
